cf6d2615b81512beed26910ec41dce1dc41d7c4e
fredericGelinat <fredericgelinat@gmail.com>
2022-02-07 01:54:18 +0100

    first commit

---
+++ js/rosace.js
@@ -0,0 +1,35 @@
+ 
+ 
+ var canvas = document.getElementById('canvas');
+ var ctx = canvas.getContext('2d');
+ canvas.width = window.innerWidth;
+ canvas.height = window.innerHeight;
+
+ var x = 0;
+ var y = 0;
+var tour = 0;
+ ctx.translate(innerWidth / 2, innerHeight / 2);
+
+function dessin(){
+    requestAnimationFrame(dessin);
+    ctx.beginPath();
+    ctx.strokeStyle = '#EDF5E1';
+    ctx.arc(x,y,100, 0, 2*Math.PI);
+    ctx.stroke();
+    ctx.rotate(30*Math.PI/ 180);
+    tour = tour + 30;
+
+    if(tour > 10000){
+        return;
+    }
+    if(tour % 360 === 0){
+        x += 10;
+        y += 10;
+    }
+}
+// document.body.style.background = 'url(' + canvas.toDataURL() + ')';
+dessin();
+
+
+
+

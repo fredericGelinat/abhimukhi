47967d9a1ad4a43e8d43cf14f02b3bccaef871d8
fredericGelinat <fredericgelinat@gmail.com>
2022-02-07 03:02:48 +0100

    responsive?

--- js/rosace.js
+++ js/rosace.js
@@ -5,30 +5,57 @@
  canvas.width = window.innerWidth;
  canvas.height = window.innerHeight;
 
- var x = 0;
- var y = 0;
+window.addEventListener('resize', function(){
+ canvas.width = window.innerWidth;
+ canvas.height = window.innerHeight;
+ init();
+});
+
+var x = 0;
+var y = 0;
 var tour = 0;
- ctx.translate(innerWidth / 2, innerHeight / 2);
+ctx.translate(innerWidth / 2, innerHeight / 2);
+// function init(){
+//     requestAnimationFrame(init);
+//     ctx.beginPath();
+//     ctx.strokeStyle = '#EDF5E1';
+//     ctx.arc(x,y,100, 0, 2*Math.PI);
+//     ctx.stroke();
+//     ctx.rotate(30*Math.PI/ 180);
+//     tour = tour + 30;
 
-function dessin(){
+//     if(tour > 10000){
+//         return;
+//     }
+//     if(tour % 360 === 0){
+//         x += 10;
+//         y += 10;
+//     }
+// }
+// init();
+
+
+
+ function dessin(){
+     
     requestAnimationFrame(dessin);
-    ctx.beginPath();
-    ctx.strokeStyle = '#EDF5E1';
-    ctx.arc(x,y,100, 0, 2*Math.PI);
-    ctx.stroke();
-    ctx.rotate(30*Math.PI/ 180);
-    tour = tour + 30;
+     ctx.beginPath();
+     ctx.strokeStyle = '#EDF5E1';
+     ctx.arc(x,y,100, 0, 2*Math.PI);
+     ctx.stroke();
+     ctx.rotate(30*Math.PI/ 180);
+     tour = tour + 30;
 
-    if(tour > 10000){
-        return;
-    }
-    if(tour % 360 === 0){
-        x += 10;
+     if(tour > 10000){
+         return;
+     }
+     if(tour % 360 === 0){
+         x += 10;
         y += 10;
     }
 }
-// document.body.style.background = 'url(' + canvas.toDataURL() + ')';
-dessin();
+// // document.body.style.background = 'url(' + canvas.toDataURL() + ')
+ dessin();
 
 
 
